a481ffee019170dedca7293898a2501e79d1a049
63070095 <73688535+63070095@users.noreply.github.com>
2020-11-27 15:06:27 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,7 +17,8 @@ $sql = "INSERT INTO guestbook (product , price , discount) VALUES ('$product', '
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "New record created successfully!";
+    echo '<a href="https://nidaweb.azurewebsites.net/show.php"> click here to open first page </a>';
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
